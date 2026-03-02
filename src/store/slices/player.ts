5d6ed39046a775f061ad71d92b7caddf9deed7d7
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2026-03-02 14:08:47 -0300

    Refactor Header and Video components to handle loading state and improve null checks

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -19,32 +19,27 @@ interface PlayerState {
   course: Course | null;
   currentModuleIndex: number;
   currentLessonIndex: number;
+  isLoading: boolean;
 }
 
 const initialState: PlayerState = {
   course: null,
   currentModuleIndex: 0,
   currentLessonIndex: 0,
+  isLoading: true
 }
 
 export const loadCourse = createAsyncThunk(
   'player/load',
   async () => {
-    api.get('/courses/1').then(response => {
-      dispatch(start(response.data))
-    })
-
+    const response = await api.get('/courses/1').then()
+    return response.data
   })
 
 export const playerSlice = createSlice({
   name: "player",
   initialState,
   reducers: {
-    start: (state, action: PayloadAction<Course>) => {
-      state.course = action.payload;
-      state.currentModuleIndex = 0;
-      state.currentLessonIndex = 0;
-    },
     play: (
       state,
       action: PayloadAction<{ moduleIndex: number; lessonIndex: number }>
@@ -67,12 +62,21 @@ export const playerSlice = createSlice({
         }
       }
     },
+  },
+  extraReducers: (builder)=> {
+     builder.addCase(loadCourse.pending, (state) => {
+      state.isLoading = true
+  })
+    builder.addCase(loadCourse.fulfilled, (state, action) => {
+      state.course = action.payload
+      state.isLoading = false
+  })
   }
 })
 
 export const player = playerSlice.reducer;
 
-export const { start, play, next } = playerSlice.actions;
+export const { play, next } = playerSlice.actions;
 
 export const useCurrentLesson = () => {
   return useAppSelector((state) => {
